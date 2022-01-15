33e58b8397d464430096224cb44f80d1087af362
Jonny-Cho <andante2183@gmail.com>
2022-01-15 17:16:54 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/commerce/CommerceOrderMsApplication.java
@@ -0,0 +1,13 @@
+package com.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceOrderMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceOrderMsApplication.class, args);
+    }
+
+}
